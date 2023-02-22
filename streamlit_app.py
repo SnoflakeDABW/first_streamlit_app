ca23a86da7105f687d473ba350325653b240f9b7
SnoflakeDABW <125316169+SnoflakeDABW@users.noreply.github.com>
2023-02-22 11:11:37 -0500

    Update streamlit_app.py
    Move the Fruit Load List Query and Load into a Button Action

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
 #create the repeatable code block (call function)
 def get_fruityvice_data(this_fruit_choice):
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
@@ -47,11 +46,18 @@ except URLError as e:
 #don't run anything past here while we troubleshoot
 streamlit.stop
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+#import snowflake.connector
 streamlit.header("The fruit load list contains:")
+#snowflake-related function
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+          my_cur.execute("SELECT * from fruit_load_list")
+          return my_cur.fetchall()
+     
+#add a button to load the fruit
+if streamlit.button('Get fruit load list'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_rows = get_fruit_load_list()
 streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list 
